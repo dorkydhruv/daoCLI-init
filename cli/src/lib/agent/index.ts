1b00d69864bddd3edbe575dfbf657aa64d19593b
Dhruv Sharma <thedhruvsharma@gmail.com>
2025-02-15 19:26:00 +0530

    a test

--- cli/src/lib/agent/index.ts
+++ cli/src/lib/agent/index.ts
@@ -28,33 +28,33 @@ class AgentManager {
     );
   }
 
-  // static async getTestInstance(): Promise<SolanaClientAgent> {
-  //   const network: "devnet" = "devnet";
-  //   const devnetRPC = "https://api.devnet.solana.com";
-  //   const connection = new Connection(devnetRPC, "confirmed");
-  //   const testKeypair = Keypair.generate();
-  //   // Request airdrop
-  //   const airdropSig = await connection.requestAirdrop(
-  //     testKeypair.publicKey,
-  //     3 * LAMPORTS_PER_SOL
-  //   );
-  //   await connection.confirmTransaction(airdropSig);
-  //   // Update keypair file so spawnSync uses the new signer
-  //   const config = loadConfig();
-  //   console.log(`Switching to ${network}`);
-  //   const keypairPath = resolve(process.cwd(), config.keypairPaths[network]);
-  //   console.log(`Writing keypair to ${keypairPath}`);
-  //   writeFileSync(
-  //     keypairPath,
-  //     JSON.stringify(Array.from(testKeypair.secretKey))
-  //   );
-  //   AgentManager.instance = new SolanaClientAgent(
-  //     network,
-  //     config.keypairPaths[network],
-  //     testKeypair
-  //   );
-  //   return AgentManager.instance;
-  // }
+  static async getTestInstance(): Promise<SolanaClientAgent> {
+    const network: "devnet" = "devnet";
+    const devnetRPC = "https://api.devnet.solana.com";
+    const connection = new Connection(devnetRPC, "confirmed");
+    const testKeypair = Keypair.generate();
+    // Request airdrop
+    const airdropSig = await connection.requestAirdrop(
+      testKeypair.publicKey,
+      3 * LAMPORTS_PER_SOL
+    );
+    await connection.confirmTransaction(airdropSig);
+    // Update keypair file so spawnSync uses the new signer
+    const config = loadConfig();
+    console.log(`Switching to ${network}`);
+    const keypairPath = resolve(process.cwd(), config.keypairPaths[network]);
+    console.log(`Writing keypair to ${keypairPath}`);
+    writeFileSync(
+      keypairPath,
+      JSON.stringify(Array.from(testKeypair.secretKey))
+    );
+    AgentManager.instance = new SolanaClientAgent(
+      network,
+      config.keypairPaths[network],
+      testKeypair
+    );
+    return AgentManager.instance;
+  }
 }
 
 export { AgentManager };
